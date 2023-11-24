fd65d2766ef37145222049f4b3c7466347bb155a
edmiralles <eddymiralles38@gmail.com>
2023-11-24 15:06:57 +0100

    css fini à 90% début JS

---
+++ script/modules/caroussel.js
@@ -0,0 +1,11 @@
+const slider = [
+	{
+		"image": "../assets/slide1.jpg",
+	},
+	{
+		"image": "../assets/slide2.jpg",
+	},
+	{
+		"image": "../assets/slide3.jpg",
+	}
+]
---
+++ script/modules/slider.js
@@ -0,0 +1,159 @@
+const carsLocation = [
+    {
+        "id": 1,
+        "image": "vehicule1.png",
+        "model":"peugeot",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.5km supplémentaire).",
+        "price": 999,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule2.png",
+            "vehicule3.png",
+            "vehicule4.png"
+        ]
+    },
+    {
+        "id": 2,
+        "image": "vehicule2.png",
+        "model":"Ford Focus",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.5km supplémentaire).",
+        "price": 999,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule3.png",
+            "vehicule4.png",
+            "vehicule1.png"
+        ]
+    },
+    {
+        "id": 3,
+        "image": "vehicule3.png",
+        "model":"Audi A1",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.55km supplémentaire).",
+        "price": 1100,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule4.png",
+            "vehicule1.png",
+            "vehicule2.png"
+        ]
+    },
+    {
+        "id": 4,
+        "image": "vehicule4.png",
+        "model":"Opel Mokka",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.4km supplémentaire).",
+        "price": 1150,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule1.png",
+            "vehicule2.png",
+            "vehicule3.png"
+        ]
+    },
+    {
+        "id": 5,
+        "image": "vehicule1.png",
+        "model":"peugeot",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.5km supplémentaire).",
+        "price": 999,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule2.png",
+            "vehicule3.png",
+            "vehicule4.png"
+        ]
+    },
+    {
+        "id": 6,
+        "image": "vehicule2.png",
+        "model":"Ford Focus",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.5km supplémentaire).",
+        "price": 999,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule3.png",
+            "vehicule4.png",
+            "vehicule1.png"
+        ]
+    },
+    {
+        "id": 7,
+        "image": "vehicule3.png",
+        "model":"Audi A1",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.55km supplémentaire).",
+        "price": 1100,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule4.png",
+            "vehicule1.png",
+            "vehicule2.png"
+        ]
+    },
+    {
+        "id": 8,
+        "image": "vehicule4.png",
+        "model":"Opel Mokka",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.4km supplémentaire).",
+        "price": 1150,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule1.png",
+            "vehicule2.png",
+            "vehicule3.png"
+        ]
+    },
+    {
+        "id": 9,
+        "image": "vehicule1.png",
+        "model":"peugeot",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.5km supplémentaire).",
+        "price": 999,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule2.png",
+            "vehicule3.png",
+            "vehicule4.png"
+        ]
+    },
+    {
+        "id": 10,
+        "image": "vehicule2.png",
+        "model":"Ford Focus",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.5km supplémentaire).",
+        "price": 999,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule3.png",
+            "vehicule4.png",
+            "vehicule1.png"
+        ]
+    },
+    {
+        "id": 11,
+        "image": "vehicule3.png",
+        "model":"Audi A1",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.55km supplémentaire).",
+        "price": 1100,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule4.png",
+            "vehicule1.png",
+            "vehicule2.png"
+        ]
+    },
+    {
+        "id": 12,
+        "image": "vehicule4.png",
+        "model":"Opel Mokka",
+        "description": "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0.4km supplémentaire).",
+        "price": 1150,
+        "agence": "Paris",
+        "thumbnails": [
+            "vehicule1.png",
+            "vehicule2.png",
+            "vehicule3.png"
+        ]
+    },
+
+]
